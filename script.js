0563801a96aa1bb79d66842f8b38e58030b1a658
murat.acar <murat.acar@live.se>
2024-01-16 13:40:30 +0100

    murat och sepideh har gjort favoriter, about, varukorg och lite js
    Co-authored-by: SepVaz <SepVaz@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-fetch('https://fakestoreapi.com/products/1')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+const cartPanel = document.getElementById("cartPanel");
+const closeCartBtn = document.getElementById("closeCartBtn");
+const cartIcon = document.querySelector(".bx-cart-alt");
+const cardHeartIcons = document.querySelectorAll(".bxs-heart");
+
+cartIcon.addEventListener("click", function () {
+    cartPanel.style.right = "0";
+});
+
+closeCartBtn.addEventListener("click", function () {
+    cartPanel.style.right = "-100%";
+});
+
+cardHeartIcons.forEach(icon => {
+    icon.addEventListener("click", function () {
+        this.style.color = this.style.color === 'red' ? '' : 'red';
+    });
+});
+
+
+
+
+
+/* fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(products => {
+        products.forEach(product => {
+            console.log({
+                id: product.id,
+                title: product.title,
+                price: product.price,
+                category: product.category,
+                description: product.description,
+                image: product.image
+            });
+        });
+    })
+    .catch(error => console.error('Error:', error)); */
+ 
